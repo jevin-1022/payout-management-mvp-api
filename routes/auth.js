2fb13f63977c14c4438183908f1a87df1555454a
Jevin Vaghasiya <jevinvaghasiya102@gmail.com>
2026-03-03 09:06:56 +0530

    Add root route

--- routes/auth.js
+++ routes/auth.js
@@ -3,4 +3,13 @@ const { login } = require("../controllers/authController");
 
 router.post("/login", login);
 
+// root route
+router.get("/", (request, response) => {
+  response.send({
+    name: "Payout-Management-MVP-API",
+    date: new Date(),
+    test: "1",
+  });
+});
+
 module.exports = router;
